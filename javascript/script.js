e8bfab66f36a4900b2412e1e3fe0aafdc76f5ebe
Antoine Landrieux <antoldrx.contact@gmail.com>
2024-10-27 14:23:12 +0100

    [misc] Remove text

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,4 @@
 
-let lastY = window.scrollY;
-let navbr = document.getElementById("navbar");
 let input = document.getElementById("search");
 
 input.addEventListener('keypress', (event) => {
@@ -10,13 +8,4 @@ input.addEventListener('keypress', (event) => {
 
 document.getElementById("search-submit").addEventListener('click', () => {
     window.find(input.value);
-});
-
-document.addEventListener('scroll', () => {
-    let y = window.scrollY;
-    if (y > lastY && !navbr.classList.contains("hide"))
-        navbr.classList.add("hide");
-    else if (y < lastY)
-        navbr.classList.remove("hide");
-    lastY = y;
 });
